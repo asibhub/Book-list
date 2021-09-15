08a9460b6151ad8e20a08dd2fb4fa42b1bdf4a3b
asibhub <asibcse@gmail.com>
2021-09-16 00:16:31 +0600

    commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const year = document.getElementById('year');
+const btn = document.getElementById('btn');
+const bookList = document.getElementById('book-list');
+
+btn.addEventListener('click', function () {
+    if (title.value == '' && author.value == '' && year.value == '') {
+        alert("Give some value")
+    }
+    else {
+        const newRow = document.createElement('tr');
+
+        const newTitle = document.createElement('th');
+        newTitle.innerHTML = title.value;
+        newRow.appendChild(newTitle);
+
+        const newAuthor = document.createElement('th');
+        newAuthor.innerHTML = author.value;
+        newRow.appendChild(newAuthor);
+
+        const newYear = document.createElement('th');
+        newYear.innerHTML = year.value;
+        newRow.appendChild(newYear);
+
+        bookList.appendChild(newRow);
+
+    }
+
+});
